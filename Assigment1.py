fae501f2736d3c227a4d25f27d7d49450307bee0
Armaghanhashmi11 <57362518+Armaghanhashmi11@users.noreply.github.com>
2019-11-04 21:32:21 +0500

    Add files via upload

---
+++ Assigment1.py
@@ -0,0 +1,106 @@
+
+
+
+
+#question 1
+
+
+
+
+print("Twinkle, twinkle, little star, \n")
+
+print("\tHow I wonder what you are! \n")
+
+print("\t\tUp above the world so high, \n")
+
+print("\t\tLike a diamond in the sky.")
+
+print("\nTwinkle, twinkle, little star,")
+
+print("\n\tHow I wonder what you are!")
+
+
+
+
+
+
+
+
+
+#question 2
+
+
+
+
+import sys
+
+print(sys.version)
+
+
+
+
+
+
+question 3
+
+
+from datetime import
+ date
+today = date.today()
+
+print("Today's date:", today)
+
+
+
+
+
+
+
+
+#question 4
+
+
+
+radius=int(input("enter radius number:"))
+
+pi=3.14
+
+area=pi*radius**2
+
+print("area is :",area)
+
+
+
+
+
+
+
+#question 5
+
+
+firstname=input("enter firstname:")
+
+lastname=input("enter lastname:")
+
+print(lastname," ",firstname)
+
+
+
+
+
+
+
+#question 6
+
+
+
+num1=float(input("enter first number:"))
+
+num2=float(input("enter second number:"))
+
+result=num1+num2
+print("addition is =",result)
+
+
+
+
